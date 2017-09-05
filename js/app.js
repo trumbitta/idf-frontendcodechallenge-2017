af0cc382668d8370ff1e9a6b6716fbad829bdd2f
William Ghelfi <trumbitta@gmail.com>
2017-09-05 18:08:18 +0200

    Protect also the main JS code with an IIFE

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    on(window, 'load', init);
+
+    function init () {
+        // Start session, load data from storage
+        // Use data to populate "Existing colleagues" and current number
+        console.log('App started');
+    }
+})();
