1553087cc7f07218d73d509bc92877bd171b56ff
William Ghelfi <trumbitta@gmail.com>
2017-09-05 17:54:22 +0200

    Start separating application files
    I needed a way to organize my code in classes / files, but `import`
    is not yet widely supported and I can't use a transpiler.
    So I'll try to make things work by attaching methods and objects to
    `window`.

---
+++ js/utils.js
@@ -0,0 +1,8 @@
+(function (window) {
+    'use strict';
+
+    // Wish I could've used a transpiler to enable `import`, classes, and avoid this
+    window.on = function (target, eventType, callback) {
+        target.addEventListener(eventType, callback);
+    }
+})(window)
