8fa6d49173b35a5d82be8699cce619ff30964516
William Ghelfi <trumbitta@gmail.com>
2017-09-05 19:52:05 +0200

    Add first stub of data storage management

---
+++ js/store.js
@@ -0,0 +1,36 @@
+(function(window) {
+    'use strict';
+
+    var localStorage = window.localStorage;
+
+    function Store(dbName) {
+        this.dbName = dbName;
+
+        // var mockData = {
+        //     count: 3,
+        //     existingUsers: [
+        //         { name: 'Donald', email: 'donald@disney.com' },
+        //         { name: 'Goofy', email: 'goofy@disney.com' },
+        //         { name: 'Mickey', email: 'mickey@disney.com' }
+        //     ]
+        // };
+        // localStorage.setItem(dbName, JSON.stringify(mockData));
+
+        if (localStorage.getItem(dbName) === null) {
+            var data = {
+                count: 0,
+                existingUsers: []
+            };
+
+            localStorage.setItem(dbName, JSON.stringify(data));
+        }
+    }
+
+    Store.prototype.findAll = function() {
+        var data = JSON.parse(localStorage.getItem(this.dbName));
+        return data;
+    };
+
+    window.app = window.app || {};
+    window.app.Store = Store;
+})(window);
