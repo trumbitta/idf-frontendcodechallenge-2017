c15f5905776ff798aee7a6e7cd7a3135a7b1d882
William Ghelfi <trumbitta@gmail.com>
2017-09-10 16:47:48 +0200

    Animate item addition and removal

--- js/components/colleagues-add/colleagues-add-list.component.js
+++ js/components/colleagues-add/colleagues-add-list.component.js
@@ -11,14 +11,22 @@
     this.template.render(templateData);
 
     this.colleaguesToAdd.forEach(function(colleague, index) {
-      var colleagueElement = document.createElement('li');
-      colleagueElement.setAttribute('class', 'form-row'); // I don't like setting classes programmatically
-      colleagueElement.setAttribute('data-template', 'colleagues-add-list-item');
+      var colleagueElement = prepareColleagueElement();
       this.template.templateTarget.appendChild(colleagueElement);
 
       this.colleaguesAddListItemComponent = new app.ColleaguesAddListItemComponent(colleague, index, this.template.templateTarget);
       this.colleaguesAddListItemComponent.updateView();
     }, this);
+  }
+
+  function prepareColleagueElement() {
+    var element = document.createElement('li');
+
+    // I don't like setting classes programmatically; but this time I couldn't come up with a better custom framework, so I have to.
+    element.classList.add('form-row', 'animate', 'fade-in');
+    element.setAttribute('data-template', 'colleagues-add-list-item');
+
+    return element;
   }
 
   window.app = window.app || {};
--- js/components/colleagues-list/colleagues-list-item.component.js
+++ js/components/colleagues-list/colleagues-list-item.component.js
@@ -25,9 +25,21 @@
       event.preventDefault();
       event.stopPropagation();
 
-      var customEvent = new CustomEvent(eventName, { detail: this.id });
-      document.dispatchEvent(customEvent); // Dispatching to `document` because several components listen to it
+      this._fadeOutItem();
     }.bind(this));
+
+    on(this.template.templateTarget, 'animationend', function(event) {
+      // Wait for the removal animation to finish, then actually emit the event asking for item removal
+      if (event.animationName === 'fade-out') { // This should be a configuration constant
+        var customEvent = new CustomEvent(eventName, { detail: this.id });
+        // Using `document` for ease of going up two levels
+        document.dispatchEvent(customEvent);
+      }
+    });
+  }
+
+  ColleaguesListItemComponent.prototype._fadeOutItem = function() {
+    this.template.templateTarget.classList.add('animate', 'fade-out');
   }
 
   window.app = window.app || {};
